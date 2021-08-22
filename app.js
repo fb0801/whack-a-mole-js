74935a4d7ee912f621d191bc6460480b2c9b6852
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-08-22 18:17:25 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+const square =document.querySelectorAll('.square') //get all the elements called sqaure
+const mole = document.querySelectorAll('.mole')//get all the mole elements
+const timeLeft = document.querySelector('#time-left')
+let score = document.querySelector('#score')
